b68bfd05c850492f285e467b157679c52f470e2d
Shana2002 <hansakaravi02@gmail.com>
2024-10-29 00:16:51 +0530

    cart class enchanced

--- lib/class/cart_class.dart
+++ lib/class/cart_class.dart
@@ -7,8 +7,23 @@ class CartClass {
   CartClass({required this.cusID});
 
   void addTocart(String id, int qty) {
-    Map _cart = {"id": id, "qty": qty};
-    cart.add(_cart);
+    int _index = 0;
+    bool isHave = false;
+
+    for (var element in cart) {
+      if (element["id"] == id) {
+        isHave = true;
+        break;
+      }
+      _index++;
+    }
+    if (isHave) {
+      cart[_index]["qty"] = cart[_index]["qty"] + qty;
+    } else {
+      Map _cart = {"id": id, "qty": qty};
+      cart.add(_cart);
+    }
+
     print(cart);
   }
 }
